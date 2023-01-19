378b9e1045ef8d18c24703a93cea4359c45f18ad
muratmnz <minazzmurat@gmail.com>
2023-01-19 23:25:20 +0300

    added fragments. updated gradle xml.

--- app/src/main/java/com/muratmnz/movieapp/MainActivity.kt
+++ app/src/main/java/com/muratmnz/movieapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.muratmnz.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.muratmnz.movieapp.ui.characters.CharactersFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
