2fe3c5c34b181e8742c145e81dae4b3e81ef8a40
muratmnz <minazzmurat@gmail.com>
2023-01-21 00:44:26 +0300

    added bottomnavigationbar feature. added icons.

--- app/src/main/java/com/muratmnz/movieapp/MainActivity.kt
+++ app/src/main/java/com/muratmnz/movieapp/MainActivity.kt
@@ -2,13 +2,25 @@ package com.muratmnz.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import com.muratmnz.movieapp.ui.characters.CharactersFragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.muratmnz.movieapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+     lateinit var binding: ActivityMainBinding
+     lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController= Navigation.findNavController(this,R.id.activity_main_nav_host_fragment)
+        setupWithNavController(binding.bottomNav,navController)
+
     }
+
 
 }
